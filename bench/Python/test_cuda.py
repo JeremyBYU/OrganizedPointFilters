c9594e6d0663e39c30a64d1873906cefde62f5d5
Jeremy Castagno <jeremybyu@gmail.com>
2020-04-20 17:28:43 -0400

    Add better cuda benchmark

--- bench/Python/test_cuda.py
+++ bench/Python/test_cuda.py
@@ -4,30 +4,33 @@ import pytest
 from examples.python.utility.helper import compute_normals_and_centroids_opc
 
 @pytest.mark.parametrize("loops", [1, 2, 3, 4, 5])
-def test_cuda_laplacian_k3(loops, benchmark):
+@pytest.mark.parametrize("size", [250, 500])
+def test_cuda_laplacian_k3(loops, size, benchmark):
     """
     Will benchmark laplacian cuda
     """
     # seems the memory transfer penalty is only REALLY high on the first iteration
-    a = np.random.rand(250, 250, 3).astype(np.float32)
+    a = np.random.rand(size, size, 3).astype(np.float32)
     b = benchmark(opf_cuda.kernel.laplacian_K3_cuda, a, loops, 0.5)
 
 @pytest.mark.parametrize("loops", [1, 2, 3, 4, 5])
-def test_cuda_laplacian_k5(loops, benchmark):
+@pytest.mark.parametrize("size", [250, 500])
+def test_cuda_laplacian_k5(loops, size, benchmark):
     """
     Will benchmark laplacian cuda
     """
     # seems the memory transfer penalty is only REALLY high on the first iteration
-    a = np.random.rand(250, 250, 3).astype(np.float32)
+    a = np.random.rand(size, size, 3).astype(np.float32)
     b = benchmark(opf_cuda.kernel.laplacian_K5_cuda, a, loops, 0.5)
 
 @pytest.mark.parametrize("loops", [1, 2, 3, 4, 5])
-def test_cuda_bilateral_k3(loops, benchmark):
+@pytest.mark.parametrize("size", [250, 500])
+def test_cuda_bilateral_k3(loops, size, benchmark):
     """
     Will benchmark laplacian cuda
     """
     # seems the memory transfer penalty is only REALLY high on the first iteration
-    a = np.random.rand(250, 250, 3).astype(np.float32)
+    a = np.random.rand(size, size, 3).astype(np.float32)
     normals_opc, centroids_opc = compute_normals_and_centroids_opc(a, convert_f64=False)
     b = benchmark(opf_cuda.kernel.bilateral_K3_cuda, normals_opc, centroids_opc, loops)
 
